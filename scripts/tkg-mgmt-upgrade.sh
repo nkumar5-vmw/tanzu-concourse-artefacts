3880a4b42a47a349318ebb52f274864aa81c05e6
Naresh Kumar M <82030210+nkumar5-vmw@users.noreply.github.com>
2021-08-18 14:24:52 +0530

    Update tkg-mgmt-upgrade.sh

--- scripts/tkg-mgmt-upgrade.sh
+++ scripts/tkg-mgmt-upgrade.sh
@@ -6,10 +6,10 @@ export MGMT_CLUSTER_KUBECONFIG_PATH="${TKG_MGMT_CLUSTER_KUBECONFIG_PATH}"
 export MGMT_CLUSTER_CONTEXT="${TKG_MGMT_CLUSTER_CONTEXT}"
 export MGMT_CLUSTER_NAME="${TKG_MGMT_CLUSTER_NAME}"
 
-tanzu login --kubeconfig ${TKG_MGMT_KUBECONFIG_PATH} \
-            --context ${TKG_MGMT_CLUSTER_CONTEXT} \
-            --name ${TKG_MGMT_CLUSTER_NAME}
-export KUBECONFIG=${TKG_MGMT_CLUSTER_KUBECONFIG_PATH}
+tanzu login --kubeconfig ${MGMT_CLUSTER_KUBECONFIG_PATH} \
+            --context ${MGMT_CLUSTER_CONTEXT} \
+            --name ${MGMT_CLUSTER_NAME}
+export KUBECONFIG=${MGMT_CLUSTER_KUBECONFIG_PATH}
 
 rm -rf ~/.tanzu/tkg/bom
 export TKG_BOM_CUSTOM_IMAGE_TAG="v1.3.1-patch1"
